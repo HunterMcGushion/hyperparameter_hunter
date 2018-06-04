6e8e1ae5d3e88d516a4b53daf1fcd75ac9461cde
Hunter McGushion <hunter@mcgushion.com>
2018-06-03 19:44:14 -0700

    autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../../hyperparameter_hunter'))
 
 # -- Project information -----------------------------------------------------
 
