33481fab32f342990fa8c137ec5f8384e40cb10d
Hunter McGushion <hunter@mcgushion.com>
2018-06-04 14:10:27 -0700

    autodoc

--- docs/source/modules.rst
+++ docs/source/modules.rst
@@ -5,3 +5,4 @@ hyperparameter_hunter
    :maxdepth: 4
 
    hyperparameter_hunter
+   setup
--- hyperparameter_hunter/environment.py
+++ hyperparameter_hunter/environment.py
@@ -62,7 +62,7 @@ class Environment():
             train_dataset,  # TODO: Allow providing separate train_input, train_target dataframes, or the full df
             environment_params_path=None,
             # *,  # FLAG: ORIGINAL
-            *args,  # FLAG: TEST AUTODOCS COMPATIBILITY
+            # *args,  # FLAG: TEST AUTODOCS COMPATIBILITY
             root_results_path=None,
             holdout_dataset=None,  # TODO: Allow providing separate holdout_input, holdout_target dataframes, or the full df
             test_dataset=None,  # TODO: Allow providing separate test_input, test_target dataframes, or the full df
