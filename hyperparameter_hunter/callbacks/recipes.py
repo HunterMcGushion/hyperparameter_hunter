fb31a2fb7009f248906940309ffb3fafcfd207d1
Hunter McGushion <hunter@mcgushion.com>
2018-09-18 20:30:10 -0700

    Update lambda_callback to infer desired attributes to pass to callables by signature. Remove required_attributes parameter, and update callback recipes accordingly

--- hyperparameter_hunter/callbacks/recipes.py
+++ hyperparameter_hunter/callbacks/recipes.py
@@ -16,50 +16,53 @@ Related
 ##################################################
 from hyperparameter_hunter.callbacks.bases import lambda_callback
 from hyperparameter_hunter.metrics import get_clean_prediction
-from hyperparameter_hunter.settings import G
-
-##################################################
-# Import Miscellaneous Assets
-##################################################
-from uuid import uuid4 as uuid
 
 ##################################################
 # Import Learning Assets
 ##################################################
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix as sk_confusion_matrix
 
 
 ##################################################
 # Confusion Matrix Callbacks
 ##################################################
 def confusion_matrix_oof(on_run=True, on_fold=True, on_repetition=True, on_experiment=True):
-    required_attributes = [
-        "train_target_data",
-        "fold_validation_target",
-        "final_oof_predictions",
-        "repetition_oof_predictions",
-        "run_validation_predictions",
-        "validation_index",
-    ]
+    """
 
-    def _on_run_end(_0, fold_validation_target, _2, _3, run_validation_predictions, _5):
-        return _confusion_matrix_helper(fold_validation_target, run_validation_predictions)
+    Parameters
+    ----------
+    on_run: Boolean, default=True
+        If False, skip making confusion matrices for individual Experiment runs
+    on_fold: Boolean, default=True
+        If False, skip making confusion matrices for individual Experiment folds
+    on_repetition: Boolean, default=True
+        If False, skip making confusion matrices for individual Experiment repetitions
+    on_experiment: Boolean, default=True
+        If False, skip making final confusion matrix for the Experiment
 
-    def _on_fold_end(
-        _0, fold_validation_target, _2, repetition_oof_predictions, _4, validation_index
-    ):
-        return _confusion_matrix_helper(
+    Returns
+    -------
+    LambdaCallback
+        An uninitialized :class:`LambdaCallback` to generate confusion matrices, produced by
+        :func:`hyperparameter_hunter.callbacks.bases.lambda_callback`"""
+    # TODO: Add documentation to this and inner helper functions
+    # TODO: Note that, unlike `confusion_matrix_holdout`, this allows `lambda_callback` to automatically aggregate stats
+
+    def _on_run_end(fold_validation_target, run_validation_predictions, **kwargs):
+        return _confusion_matrix(fold_validation_target, run_validation_predictions)
+
+    def _on_fold_end(fold_validation_target, repetition_oof_predictions, validation_index):
+        return _confusion_matrix(
             fold_validation_target, repetition_oof_predictions.iloc[validation_index]
         )
 
-    def _on_repetition_end(train_target_data, _1, _2, repetition_oof_predictions, _4, _5):
-        return _confusion_matrix_helper(train_target_data, repetition_oof_predictions)
+    def _on_repetition_end(train_target_data, repetition_oof_predictions):
+        return _confusion_matrix(train_target_data, repetition_oof_predictions)
 
-    def _on_experiment_end(train_target_data, _1, final_oof_predictions, _3, _4, _5):
-        return _confusion_matrix_helper(train_target_data, final_oof_predictions)
+    def _on_experiment_end(train_target_data, final_oof_predictions):
+        return _confusion_matrix(train_target_data, final_oof_predictions)
 
     return lambda_callback(
-        required_attributes=required_attributes,
         on_run_end=_on_run_end if on_run else None,
         on_fold_end=_on_fold_end if on_fold else None,
         on_repetition_end=_on_repetition_end if on_repetition else None,
@@ -68,84 +71,55 @@ def confusion_matrix_oof(on_run=True, on_fold=True, on_repetition=True, on_exper
     )
 
 
-def confusion_matrix_holdout():
-    required_attributes = [
-        "stat_aggregates",
-        "holdout_target_data",
-        "final_holdout_predictions",
-        "repetition_holdout_predictions",
-        "fold_holdout_predictions",
-        "run_holdout_predictions",
-    ]
+def confusion_matrix_holdout(on_run=True, on_fold=True, on_repetition=True, on_experiment=True):
+    # TODO: Add documentation to this and inner helper functions
+    # TODO: Note that, unlike `confusion_matrix_oof`, this manually handles aggregating stats
 
-    def _on_experiment_start(
-        stat_aggregates,
-        holdout_target_data,
-        final_holdout_predictions,
-        repetition_holdout_predictions,
-        fold_holdout_predictions,
-        run_holdout_predictions,
-    ):
+    def _on_experiment_start(stat_aggregates):
         stat_aggregates["confusion_matrix_holdout"] = dict(runs=[], folds=[], reps=[], final=None)
 
-    def _on_run_end(
-        stat_aggregates,
-        holdout_target_data,
-        final_holdout_predictions,
-        repetition_holdout_predictions,
-        fold_holdout_predictions,
-        run_holdout_predictions,
-    ):
+    def _on_run_end(stat_aggregates, holdout_target_data, run_holdout_predictions):
         stat_aggregates["confusion_matrix_holdout"]["runs"].append(
-            _confusion_matrix_helper(holdout_target_data, run_holdout_predictions)
+            _confusion_matrix(holdout_target_data, run_holdout_predictions)
         )
-        return "foo _on_run_end"
 
-    def _on_fold_end(
-        stat_aggregates,
-        holdout_target_data,
-        final_holdout_predictions,
-        repetition_holdout_predictions,
-        fold_holdout_predictions,
-        run_holdout_predictions,
-    ):
+    def _on_fold_end(stat_aggregates, holdout_target_data, fold_holdout_predictions):
         stat_aggregates["confusion_matrix_holdout"]["folds"].append(
-            _confusion_matrix_helper(holdout_target_data, fold_holdout_predictions)
+            _confusion_matrix(holdout_target_data, fold_holdout_predictions)
         )
 
-    def _on_repetition_end(
-        stat_aggregates,
-        holdout_target_data,
-        final_holdout_predictions,
-        repetition_holdout_predictions,
-        fold_holdout_predictions,
-        run_holdout_predictions,
-    ):
+    def _on_repetition_end(stat_aggregates, holdout_target_data, repetition_holdout_predictions):
         stat_aggregates["confusion_matrix_holdout"]["reps"].append(
-            _confusion_matrix_helper(holdout_target_data, repetition_holdout_predictions)
+            _confusion_matrix(holdout_target_data, repetition_holdout_predictions)
         )
 
-    def _on_experiment_end(
-        stat_aggregates,
-        holdout_target_data,
-        final_holdout_predictions,
-        repetition_holdout_predictions,
-        fold_holdout_predictions,
-        run_holdout_predictions,
-    ):
-        stat_aggregates["confusion_matrix_holdout"]["final"] = _confusion_matrix_helper(
+    def _on_experiment_end(stat_aggregates, holdout_target_data, final_holdout_predictions):
+        stat_aggregates["confusion_matrix_holdout"]["final"] = _confusion_matrix(
             holdout_target_data, final_holdout_predictions
         )
 
     return lambda_callback(
-        required_attributes=required_attributes,
         on_experiment_start=_on_experiment_start,
-        on_run_end=_on_run_end,
-        on_fold_end=_on_fold_end,
-        on_repetition_end=_on_repetition_end,
-        on_experiment_end=_on_experiment_end,
+        on_run_end=_on_run_end if on_run else None,
+        on_fold_end=_on_fold_end if on_fold else None,
+        on_repetition_end=_on_repetition_end if on_repetition else None,
+        on_experiment_end=_on_experiment_end if on_experiment else None,
+        agg_name="confusion_matrix_holdout",
     )
 
 
-def _confusion_matrix_helper(targets, predictions):
-    return confusion_matrix(targets, get_clean_prediction(targets, predictions))
+def _confusion_matrix(targets, predictions):
+    """Helper function to produce a confusion matrix after properly formatting `predictions`
+
+    Parameters
+    ----------
+    targets: Array-like
+        The target/expected output labels for each of the given `predictions`
+    predictions: Array-like
+        The predicted values corresponding to each of the elements in `targets`
+
+    Returns
+    -------
+    Array-like
+        A confusion matrix for the given `targets` and `predictions`"""
+    return sk_confusion_matrix(targets, get_clean_prediction(targets, predictions))
