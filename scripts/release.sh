2a218f2aa4ff57d6f531979444d15bbfe44f6e71
Hunter McGushion <hunter@mcgushion.com>
2018-11-25 16:15:10 -0800

    Update release script to ask for confirmation that necessary supporting files have been updated

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,23 +5,32 @@
 ##################################################
 MY_VERSION=`cat hyperparameter_hunter/VERSION`
 
-##################################################
-# Start Release Process
-##################################################
-# Assume at least the necessary release files (VERSION, CHANGELOG.md) have been `git add`-ed
-git commit -m "Upload v$MY_VERSION to PyPI"
-git tag "v$MY_VERSION"
+echo "Have the necessary release files (VERSION, CHANGELOG) been updated and \`git add\`-ed?"
+read -p "Press 'y' to continue, or 'n' to abort... " -n 1 -r
+echo
 
-##################################################
-# Upload to PyPI
-##################################################
-python setup.py sdist bdist_wheel
-twine upload dist/*
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    ##################################################
+    # Start Release Process
+    ##################################################
+    # Assume at least the necessary release files (VERSION, CHANGELOG.md) have been `git add`-ed
+    git commit -m "Upload v$MY_VERSION to PyPI"
+    git tag "v$MY_VERSION"
 
-##################################################
-# Upload to Repository
-##################################################
-git push origin HEAD
-git push origin --tags
+    ##################################################
+    # Upload to PyPI
+    ##################################################
+    python setup.py sdist bdist_wheel
+    twine upload dist/*
 
-echo "Please finalize release on GitHub with supporting PyPI files"
+    ##################################################
+    # Upload to Repository
+    ##################################################
+    git push origin HEAD
+    git push origin --tags
+
+    echo "Please finalize release on GitHub with supporting PyPI files"
+else
+    echo "Aborting release"
+fi
+
