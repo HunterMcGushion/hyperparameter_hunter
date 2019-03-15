8be9664de7fb53a90c1e33ae277bb98ada5e76f9
Hunter McGushion <hunter@mcgushion.com>
2019-03-14 21:03:07 -0700

    Add integration tests for `CVExperiment` with "intra_cv" `FeatureEngineer`
    - Adds simple tests for single-step `FeatureEngineer` instances that modify
    `validation_inputs` "intra_cv"

--- tests/test_feature_engineering.py
+++ tests/test_feature_engineering.py
@@ -2,6 +2,7 @@
 # Import Own Assets
 ##################################################
 from hyperparameter_hunter import Environment, CVExperiment
+from hyperparameter_hunter.callbacks.recipes import dataset_recorder
 from hyperparameter_hunter.feature_engineering import FeatureEngineer, merge_dfs, split_merged_df
 from hyperparameter_hunter.feature_engineering import DatasetNameReport, validate_dataset_names
 from hyperparameter_hunter.feature_engineering import EngineerStep
@@ -76,6 +77,20 @@ def standard_scale_0(train_inputs, holdout_inputs):
     train_inputs[train_inputs.columns] = scaler.fit_transform(train_inputs.values)
     holdout_inputs[train_inputs.columns] = scaler.transform(holdout_inputs.values)
     return train_inputs, holdout_inputs
+
+
+def standard_scale_1(train_inputs, validation_inputs):
+    scaler = StandardScaler()
+    train_inputs[train_inputs.columns] = scaler.fit_transform(train_inputs.values)
+    validation_inputs[train_inputs.columns] = scaler.transform(validation_inputs.values)
+    return train_inputs, validation_inputs
+
+
+def standard_scale_2(train_inputs, non_train_inputs):
+    scaler = StandardScaler()
+    train_inputs[train_inputs.columns] = scaler.fit_transform(train_inputs.values)
+    non_train_inputs[train_inputs.columns] = scaler.transform(non_train_inputs.values)
+    return train_inputs, non_train_inputs
 
 
 def test_0():
@@ -486,11 +501,73 @@ def test_dataset_name_report(params, stage, merged_datasets, coupled_datasets, l
 
 
 ##################################################
-# `CVExperiment` with `FeatureEngineer`
+# `CVExperiment` with `FeatureEngineer` Integration
 ##################################################
 # noinspection PyUnusedLocal
 def holdout_first_row(train_dataset, target_column):
     return train_dataset.iloc[1:5, :], train_dataset.iloc[[0], :]
+
+
+small_toy_dataset = pd.DataFrame(
+    data=[[1, -1, 2, 1], [2, 0, 0, 0], [0, 1, -1, 1], [-1, 1, 0, 0]], columns=["a", "b", "c", "t"]
+)
+
+initial_fold_train_inputs = [
+    pd.DataFrame([[1, -1, 2], [2, 0, 0], [-1, 1, 0]], columns=list("abc"), index=[0, 1, 3]),
+    pd.DataFrame([[2, 0, 0], [0, 1, -1], [-1, 1, 0]], columns=list("abc"), index=[1, 2, 3]),
+    pd.DataFrame([[1, -1, 2], [0, 1, -1], [-1, 1, 0]], columns=list("abc"), index=[0, 2, 3]),
+    pd.DataFrame([[1, -1, 2], [2, 0, 0], [0, 1, -1]], columns=list("abc"), index=[0, 1, 2]),
+]
+engineered_fold_train_inputs = [
+    pd.DataFrame(
+        [
+            [0.26726124, -1.22474487, 1.41421356],
+            [1.06904497, 0.0, -0.70710678],
+            [-1.33630621, 1.22474487, -0.70710678],
+        ],
+        columns=list("abc"),
+        index=[0, 1, 3],
+    ),
+    pd.DataFrame(
+        [
+            [1.33630621, -1.41421356, 0.70710678],
+            [-0.26726124, 0.70710678, -1.41421356],
+            [-1.06904497, 0.70710678, 0.70710678],
+        ],
+        columns=list("abc"),
+        index=[1, 2, 3],
+    ),
+    pd.DataFrame(
+        [
+            [1.22474487, -1.41421356, 1.33630621],
+            [0.0, 0.70710678, -1.06904497],
+            [-1.22474487, 0.70710678, -0.26726124],
+        ],
+        columns=list("abc"),
+        index=[0, 2, 3],
+    ),
+    pd.DataFrame(
+        [
+            [0.0, -1.22474487, 1.33630621],
+            [1.22474487, 0.0, -0.26726124],
+            [-1.22474487, 1.22474487, -1.06904497],
+        ],
+        columns=list("abc"),
+        index=[0, 1, 2],
+    ),
+]
+initial_fold_validation_inputs = [
+    pd.DataFrame([[0, 1, -1]], columns=list("abc"), index=[2]),
+    pd.DataFrame([[1, -1, 2]], columns=list("abc"), index=[0]),
+    pd.DataFrame([[2, 0, 0]], columns=list("abc"), index=[1]),
+    pd.DataFrame([[-1, 1, 0]], columns=list("abc"), index=[3]),
+]
+engineered_fold_validation_inputs = [
+    pd.DataFrame([[-0.53452248, 1.22474487, -1.76776695]], columns=list("abc"), index=[2]),
+    pd.DataFrame([[0.53452248, -3.53553391, 4.94974747]], columns=list("abc"), index=[0]),
+    pd.DataFrame([[2.44948974, -0.35355339, -0.26726124]], columns=list("abc"), index=[1]),
+    pd.DataFrame([[-2.44948974, 1.22474487, -0.26726124]], columns=list("abc"), index=[3]),
+]
 
 
 @pytest.fixture()
@@ -501,6 +578,18 @@ def toy_environment_fixture():
         metrics=["roc_auc_score"],
         target_column="class",
         cv_params=dict(n_splits=3, shuffle=True, random_state=32),
+    )
+
+
+@pytest.fixture()
+def dataset_recorder_env(request):
+    return Environment(
+        train_dataset=small_toy_dataset,
+        holdout_dataset=getattr(request, "param", None),
+        metrics=["accuracy_score"],
+        target_column="t",
+        cv_params=dict(n_splits=4, shuffle=True, random_state=32),
+        experiment_callbacks=[dataset_recorder()],
     )
 
 
@@ -522,6 +611,20 @@ def experiment_prep_fixture(request):
     # noinspection PyProtectedMember
     experiment._initial_preprocessing()
 
+    return experiment
+
+
+@pytest.fixture()
+def experiment_fixture(request):
+    #################### Build `feature_engineer` ####################
+    feature_engineer = FeatureEngineer(steps=request.param)
+
+    #################### Execute `CVExperiment` ####################
+    experiment = CVExperiment(
+        model_initializer=AdaBoostClassifier,
+        model_init_params=dict(),
+        feature_engineer=feature_engineer,
+    )
     return experiment
 
 
@@ -614,6 +717,30 @@ def test_feature_engineer_experiment(toy_environment_fixture, experiment_prep_fi
     assert_frame_equal(experiment_prep_fixture.train_target_data, end_data[1], check_dtype=False)
     assert_frame_equal(experiment_prep_fixture.holdout_input_data, end_data[2], check_dtype=False)
     assert_frame_equal(experiment_prep_fixture.holdout_target_data, end_data[3], check_dtype=False)
+
+
+@pytest.mark.parametrize(
+    "experiment_fixture", [[standard_scale_1], [standard_scale_2]], indirect=True
+)
+@pytest.mark.parametrize(
+    "end_data",
+    [
+        [
+            initial_fold_train_inputs,
+            initial_fold_validation_inputs,
+            engineered_fold_train_inputs,
+            engineered_fold_validation_inputs,
+        ]
+    ],
+)
+def test_intra_cv_engineer_experiment(dataset_recorder_env, experiment_fixture, end_data):
+    for f, datasets in enumerate(experiment_fixture.stat_aggregates["_datasets"]["on_fold_start"]):
+        assert_frame_equal(datasets["fold_train_input"], end_data[0][f])
+        assert_frame_equal(datasets["fold_validation_input"], end_data[1][f])
+
+    for f, datasets in enumerate(experiment_fixture.stat_aggregates["_datasets"]["on_fold_end"]):
+        assert_frame_equal(datasets["fold_train_input"], end_data[2][f])
+        assert_frame_equal(datasets["fold_validation_input"], end_data[3][f])
 
 
 ##################################################
