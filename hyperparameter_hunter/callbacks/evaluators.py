aa4f65243048e2446ff792096c872b6e51794a80
Hunter McGushion <hunter@mcgushion.com>
2019-04-04 23:01:18 -0700

    Update `evaluators` to use `datasets`
    - Does not yet fully support target transformations/inversions

--- hyperparameter_hunter/callbacks/evaluators.py
+++ hyperparameter_hunter/callbacks/evaluators.py
@@ -12,6 +12,7 @@ Related
 # Import Own Assets
 ##################################################
 from hyperparameter_hunter.callbacks.bases import BaseEvaluatorCallback
+from hyperparameter_hunter.datasets import Dataset
 
 ##################################################
 # Import Miscellaneous Assets
@@ -21,65 +22,57 @@ from typing import Union
 
 
 class EvaluatorOOF(BaseEvaluatorCallback):
-    fold_validation_target: pd.DataFrame
-    final_oof_predictions: pd.DataFrame
-    repetition_oof_predictions: pd.DataFrame
-    run_validation_predictions: pd.DataFrame
+    data_oof: Dataset
     validation_index: list
-    transformed_repetition_oof_target: pd.DataFrame
-    transformed_final_oof_target: pd.DataFrame
 
     def on_run_end(self):
         """Evaluate out-of-fold predictions for the run"""
-        self.evaluate("oof", self.fold_validation_target, self.run_validation_predictions)
+        self.evaluate("oof", self.data_oof.target.fold, self.data_oof.prediction.run)
         super().on_run_end()
 
     def on_fold_end(self):
         """Evaluate (run-averaged) out-of-fold predictions for the fold"""
         self.evaluate(
             "oof",
-            self.fold_validation_target,
-            self.repetition_oof_predictions.iloc[self.validation_index],
+            self.data_oof.target.fold,
+            self.data_oof.prediction.rep.iloc[self.validation_index],
         )
         super().on_fold_end()
 
     def on_repetition_end(self):
         """Evaluate (run-averaged) out-of-fold predictions for the repetition"""
         try:
-            self.evaluate(
-                "oof", self.transformed_repetition_oof_target, self.repetition_oof_predictions
-            )
+            self.evaluate("oof", self.data_oof.T.target.rep, self.data_oof.prediction.rep)
         except (TypeError, AttributeError):
-            self.evaluate("oof", self.train_target_data, self.repetition_oof_predictions)
+            # self.evaluate("oof", self.data_oof.target.d, self.data_oof.prediction.rep)  # TODO: Should use this when collecting transformed targets
+            self.evaluate(
+                "oof", self.data_train.target.d, self.data_oof.prediction.rep
+            )  # FLAG: TEMPORARY
         super().on_repetition_end()
 
     def on_experiment_end(self):
         """Evaluate final (run/repetition-averaged) out-of-fold predictions"""
         try:
-            self.evaluate("oof", self.transformed_final_oof_target, self.final_oof_predictions)
+            self.evaluate("oof", self.data_oof.T.target.final, self.data_oof.prediction.final)
         except (TypeError, AttributeError):
-            self.evaluate("oof", self.train_target_data, self.final_oof_predictions)
+            # self.evaluate("oof", self.data_oof.target.d, self.data_oof.prediction.final)  # TODO: Should use this when collecting transformed targets
+            self.evaluate(
+                "oof", self.data_train.target.d, self.data_oof.prediction.final
+            )  # FLAG: TEMPORARY
         super().on_experiment_end()
 
 
 class EvaluatorHoldout(BaseEvaluatorCallback):
-    fold_holdout_target: pd.DataFrame
-    holdout_target_data: pd.DataFrame
-    final_holdout_predictions: pd.DataFrame
-    repetition_holdout_predictions: pd.DataFrame
-    fold_holdout_predictions: pd.DataFrame
-    run_holdout_predictions: pd.DataFrame
-    transformed_repetition_holdout_target: Union[pd.DataFrame, int]
-    transformed_final_holdout_target: Union[pd.DataFrame, int]
+    data_holdout: Dataset
 
     def on_run_end(self):
         """Evaluate holdout predictions for the run"""
-        self.evaluate("holdout", self.fold_holdout_target, self.run_holdout_predictions)
+        self.evaluate("holdout", self.data_holdout.target.fold, self.data_holdout.prediction.run)
         super().on_run_end()
 
     def on_fold_end(self):
         """Evaluate (run-averaged) holdout predictions for the fold"""
-        self.evaluate("holdout", self.fold_holdout_target, self.fold_holdout_predictions)
+        self.evaluate("holdout", self.data_holdout.target.fold, self.data_holdout.prediction.fold)
         super().on_fold_end()
 
     def on_repetition_end(self):
@@ -88,12 +81,10 @@ class EvaluatorHoldout(BaseEvaluatorCallback):
             # If no target transformation occurred, `transformed_repetition_holdout_target` will
             # be `0`, raising `TypeError`
             self.evaluate(
-                "holdout",
-                self.transformed_repetition_holdout_target,
-                self.repetition_holdout_predictions,
+                "holdout", self.data_holdout.T.target.rep, self.data_holdout.prediction.rep
             )
         except (TypeError, AttributeError):
-            self.evaluate("holdout", self.holdout_target_data, self.repetition_holdout_predictions)
+            self.evaluate("holdout", self.data_holdout.target.d, self.data_holdout.prediction.rep)
 
         super().on_repetition_end()
 
@@ -103,10 +94,16 @@ class EvaluatorHoldout(BaseEvaluatorCallback):
             # If no target transformation occurred, `transformed_final_holdout_target` will
             # be `0`, raising `TypeError`
             self.evaluate(
-                "holdout", self.transformed_final_holdout_target, self.final_holdout_predictions
+                "holdout",
+                self.data_holdout.T.target.d,
+                self.data_holdout.prediction.final  # FLAG: TEMPORARY
+                # "holdout", self.holdout_data.T.target.final, self.data_holdout.prediction.final  # TODO: Should use this when collecting transformed targets
             )
         except (TypeError, AttributeError):
-            self.evaluate("holdout", self.holdout_target_data, self.final_holdout_predictions)
+            self.evaluate(
+                "holdout", self.data_holdout.target.d, self.data_holdout.prediction.final
+            )  # FLAG: TEMPORARY
+            # self.evaluate("holdout", self.data_holdout.target.final, self.data_holdout.prediction.final)  # TODO: Should use this when collecting transformed targets
 
         super().on_experiment_end()
 
