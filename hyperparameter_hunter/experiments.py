e3b56bcb2df593bff6d30b81f7e24dad53c6d38c
Hunter McGushion <hunter@mcgushion.com>
2019-04-05 17:33:17 -0700

    Add missing apostrophe in module docstring

--- hyperparameter_hunter/experiments.py
+++ hyperparameter_hunter/experiments.py
@@ -17,7 +17,7 @@ Related
 Notes
 -----
 As mentioned above, the inner workings of :mod:`experiments` will be very confusing without a grasp
-on whats going on in :mod:`experiment_core`, and its related modules"""
+on what's going on in :mod:`experiment_core`, and its related modules"""
 ##################################################
 # Import Own Assets
 ##################################################
