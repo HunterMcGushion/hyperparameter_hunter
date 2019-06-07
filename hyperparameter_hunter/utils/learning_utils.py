5041c1824c919f18669bb471252944e31ecf1782
Hunter McGushion <hunter@mcgushion.com>
2019-06-06 21:52:49 -0700

    Add `get_pima_indians_data` utility function

--- hyperparameter_hunter/utils/learning_utils.py
+++ hyperparameter_hunter/utils/learning_utils.py
@@ -18,7 +18,7 @@ def get_breast_cancer_data(target="diagnosis"):
 
     Parameters
     ----------
-    target: String, default='diagnosis'
+    target: String, default="diagnosis"
         What to name the column in `df` that contains the target output values
 
     Returns
@@ -31,12 +31,58 @@ def get_breast_cancer_data(target="diagnosis"):
     return df
 
 
+def get_pima_indians_data(target="class"):
+    """Get the Pima Indians Diabetes binary classification dataset, formatted as a DataFrame
+
+    Parameters
+    ----------
+    target: String, default="class"
+        What to name the column in `df` that contains the target output values
+
+    Returns
+    -------
+    df: `pandas.DataFrame`
+        The Pima Indians dataset, of shape (768, 8 + 1), with column names of:
+            "pregnancies",
+            "glucose",
+            "bp" (shortened from "blood_pressure"),
+            "skin_thickness",
+            "insulin",
+            "bmi",
+            "dpf" (shortened from "diabetes_pedigree_function"),
+            "age",
+            "class" (or given `target` column)
+
+    Notes
+    -----
+    This dataset is originally from the National Institute of Diabetes and Digestive and Kidney
+    Diseases. Thanks to Jason Brownlee (of MachineLearningMastery.com), who has generously made a
+    public repository to collect copies of all the datasets he uses in his tutorials
+
+    Examples
+    --------
+    >>> get_pima_indians_data().head()
+       pregnancies  glucose  bp  skin_thickness  insulin   bmi    dpf  age  class
+    0            6      148  72              35        0  33.6  0.627   50      1
+    1            1       85  66              29        0  26.6  0.351   31      0
+    2            8      183  64               0        0  23.3  0.672   32      1
+    3            1       89  66              23       94  28.1  0.167   21      0
+    4            0      137  40              35      168  43.1  2.288   33      1
+    """
+    input_cols = ["pregnancies", "glucose", "bp", "skin_thickness", "insulin", "bmi", "dpf", "age"]
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv",
+        names=(input_cols + [target]),
+    )
+    return df
+
+
 def get_diabetes_data(target="progression"):
     """Get the SKLearn Diabetes regression dataset, formatted as a DataFrame
 
     Parameters
     ----------
-    target: String, default='progression'
+    target: String, default="progression"
         What to name the column in `df` that contains the target output values
 
     Returns
